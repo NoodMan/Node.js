c0c5df4891f35e8067be8e99f69d23605404d02d
NoodMan <camara.adeline@yahoo.fr>
2022-04-29 16:21:00 +0200

    it works

--- server.js
+++ server.js
@@ -27,22 +27,33 @@ app.get("/articles", (req, res) => { // pour l'affichage la route
     //  console.log('-->⛔️ Je suis ici get🎉 <--');
     res.json(inventory)
 })
-// app.get("/modify", (req, res) => { // pour l'affichage la route 
-//     //  console.log('-->⛔️ Je suis ici get🎉 <--');
-//     res.json(inventory)
-// })
 
-app.post("/articles", (req, res) => { // pour faire des modifications
+
+app.post("/modify", (req, res) => { // pour faire des modifications
     const id = Number(req.body.id) // recuparation des valeurs
     //  console.log('-->⛔️ Je suis ici post 🎉 <--');
     const title = req.body.title
+    const description = req.body.description
+    const price = req.body.price
+    const currency = req.body.currency
+    const brand = req.body.brand
 
     let articleFound = {}; //création variable objet vide
-    
+
     inventory.articles.forEach((article) => { //pour trouver l'article qui correcpond à la requete
-        if (article.id === id)  { // verification
-            article.title = title // assignation  et modification valeur du title dans copie (articleFound) ok new title
-            articleFound = article // remplacement [] = [new id...]
+        if (article.id === id) { // verification
+            if (article.title !== title) { //verification si different
+                article.title = title // assignation  et modification valeur du title dans copie (articleFound) ok new title
+            } if (article.description !== description) {
+                article.description = description
+            } if (article.price !== price) {
+                article.price = price
+            } if (article.currency !== currency) {
+                article.currency = currency
+            } if (article.brand !== brand) {
+                article.brand = brand
+            }
+            articleFound = article // remplacement [] = [new ]
         };
     })
     if (articleFound === null) { //verification 
@@ -50,7 +61,7 @@ app.post("/articles", (req, res) => { // pour faire des modifications
     }
 
     fs.writeFileSync("./inventory.json", JSON.stringify(inventory)) //ecriture dans le fichier avec le new contenue
-    
+
     res.json(articleFound) // pour renvoie l'article modifier
 })
 
